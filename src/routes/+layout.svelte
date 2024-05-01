e31bd8b88a28c7c73a081420bc367d33a9d62ed5
hammsik <asbbrp@gmail.com>
2024-05-01 12:28:08 +0900

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script>
+	import '../app.css';
+</script>
+
+<div class="w-[100dvw] h-[100dvh] flex justify-center items-center bg-custom-primary">
+    <slot />
+</div>
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    extend: {
+      colors: {
+        custom: {
+          primary: '#2b3b55',
+          secondary: '#E3DDDC',
+          tertiary: '#C3C3C3',
+
+
+        }
+      },
+    },
+    plugins: [],
+  }
+};
